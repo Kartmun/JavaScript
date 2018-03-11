a3e16c7ee99c3a5813b4ef176610ba30371503a2
Kirill Lapin <Kirilll621@gmail.com>
2018-03-11 11:13:00 +0300

    V1.1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //Это сейчас на ГитХабе =)
+//Это снова я 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
